5b9f1b426d04ca083ef3115cb381a08939b21468
Elelctronics <electronics@ruby.local>
2022-01-04 15:59:43 +0100

    updated gas monitoring

--- README.md
+++ README.md
@@ -1,1 +1,18 @@
-# gas-detector
+# Gas Detector
+
+## Description
+
+Gas detector is a project which demonstrates the use of **MQ-2 Gas Sensor** and **ESP8266 Wemos D1 R2** in detecting gas leak
+
+## Dependencies
+
+While developing this project, the following softwares and exterbal libraries were used.
+
+1. [Arduino IDE v1.8.13](https://www.arduino.cc/en/software)
+
+2. [Adafruit GFX library](https://github.com/adafruit/Adafruit-GFX-Library)
+3. [Adafruit ST7735 library](https://github.com/adafruit/Adafruit-ST7735-Library)
+
+## Circuit Diagram
+
+Will be updated soon
--- project_main/project_main.ino
+++ project_main/project_main.ino
@@ -3,141 +3,135 @@
 #include <Adafruit_GFX.h>    // Core graphics library
 #include <Adafruit_ST7735.h> // Hardware-specific library for ST7735
 #include <SPI.h>
-//#include <Fonts/user/OswaldVariable8pt7b.h> // Use a custom font
-//#include <Fonts/user/UbuntuLightItalic9pt7b.h> // Use a custom font
-//#include <Fonts/user/UbuntuRegular8pt7b.h> // Use a custom font
+
+//NWONU CHUKWUEMEKA SIMON/
+//Reg no: 2017030182221/
+
+
+#include "OswaldRegular12pt7b.h"
+#include "OswaldRegular10pt7b.h"
+
+// Display shiled pin configs
+#define TFT_CS         D8
+#define TFT_RST        D0
+#define TFT_DC         D6
+#define TFT_MOSI       D7
+#define TFT_SCLK       D5
+// color definitions
+const uint16_t BLACK_COLOR        = 0x0000;
+const uint16_t BLUE_COLOR         = 0xF800;
+const uint16_t RED_COLOR          = 0x001F;
+const uint16_t GREEN_COLOR        = 0x07E0;
+const uint16_t CYAN_COLOR         = 0x07FF;
+const uint16_t MAGENTA_COLOR      = 0xF81F;
+const uint16_t YELLOW_COLOR       = 0xFFE0;
+const uint16_t WHITE_COLOR        = 0xFFFF;
+
 
 // Variable definitions
-int sensorValue;
-int maxValue =4;
-char oldSensorValue[4] = {0};
-int threshold =300;
-int toneDuration = 100;
-int toneFrequency = 1200;
+const size_t max_length = 6;
+unsigned long sensorValue;  //variable to store sensor value
+
+char oldSensorValue[max_length] = {0};
+
 
 // Pin definitions
-#define sensorPin A0
-int buzzerPin = 6;
-#define redLED A1
-#define greenLED A2
-#define blueLED A3
-
-// color definitions
-const uint16_t  Black        = 0x0000;
-const uint16_t  Blue         = 0x001F;
-const uint16_t  Red          = 0xF800;
-const uint16_t  Green        = 0x07E0;
-const uint16_t  Cyan         = 0x07FF;
-const uint16_t  Magenta      = 0xF81F;
-const uint16_t  Yellow       = 0xFFE0;
-const uint16_t  White        = 0xFFFF;
-
-// Pin definfitions 
-
-  #define TFT_CS        10
-  #define TFT_RST        9 // Or set to -1 and connect to Arduino RESET pin
-  #define TFT_DC         8
+#define mq2sensorPin A0
 
 
-  // Create the TFT display object
-
-  Adafruit_ST7735 tft = Adafruit_ST7735(TFT_CS, TFT_DC, TFT_RST);
-
-  void setup()
-  {
-    pinMode(redLED, OUTPUT);
-    pinMode(greenLED, OUTPUT);
-    pinMode(blueLED, OUTPUT);
-    pinMode(buzzerPin, OUTPUT);
-    
-    // Init ST7735R chip, green tab
-    tft.initR(INITR_144GREENTAB); 
-
-    // initialise the display
-    tft.setFont();
-    tft.fillScreen(Red);
-    tft.setTextColor(White);
-    Serial.begin(9600);
 
 
-  }
+// Create the TFT display object
+Adafruit_ST7735 tft = Adafruit_ST7735(TFT_CS, TFT_DC, TFT_MOSI, TFT_SCLK, TFT_RST);
 
-  //Function to activate the buzzer 
-  void raiseAlarm()
-  {
-    tone(buzzerPin, toneFrequency, toneDuration);
-  }
+void welcomeScreen(void){
 
-  void loop()
-  {
-    tft.setTextSize(1);
-    tft.setCursor(5,15);
-    tft.print("NET: ");
-    tft.print(90);
-    tft.println("%");
-    tft.setCursor(15,40);
-    tft.print("Sensor Value:");
+        //    Setup display
+        tft.initR(INITR_GREENTAB);
+        tft.fillScreen(BLUE_COLOR);
+        tft.setFont(&OswaldRegular10pt7b);
+        tft.setTextSize(1);
+        tft.setTextColor(WHITE_COLOR);
+        tft.setCursor(15,20);
+        tft.println("PROJECT BY");
+        tft.println("     NWONU");
+        tft.println(" CHUKWUEMEKA");
+        tft.println("      SIMON");
+        tft.println("2017030182221");
+        delay(5000);
+}
 
-   readSensorValue();
+void setup()
+{
+        Serial.begin(9600);
+        // Init ST7735R chip, green tab
+        tft.initR(INITR_144GREENTAB);
+        // set fontname
+        welcomeScreen();
+        tft.fillScreen(BLUE_COLOR);
+}
 
-  }
-  
 
-  void readSensorValue()
-  {
+void loop()
+{
+        readSensorValue();
 
-    /* This function tend to solve the issue with the short blinking screen 
-     *  When tft.fillScreen, This issue was solved my ovewriting the recent 
-     *  Value by a copy of it with the background color as display color
-     */
+}
 
-    
-    //Read the current sensor value
-    sensorValue = analogRead(sensorPin);
-    // Initialize the new and old sensor values
 
-    char newSensorValue[maxValue] = {0};
-    // Construct the new sensor value
-     sprintf(newSensorValue, "%d", sensorValue);
-     // Compare the two strings to see if it has changed
-    if (strcmp (oldSensorValue,newSensorValue) != 0)
-    {
-      // Change the text size
-      tft.setTextSize(2);
-      tft.setCursor(35,70);
-      // Change text color to current background color
-      tft.setTextColor(Red);
-      // Print the old value that will overwrite our current text
-      tft.print(oldSensorValue);
-      //Bring the cursor back to the starting point
-      tft.setCursor(35,70);
-      //Change the text size
-      tft.setTextSize(2);
-      // Change the text color to the foreground color
-      tft.setTextColor(White);
-      //Print the new sensor value
-      tft.print(newSensorValue);
-      // Copy the new value to old value
-      strcpy(oldSensorValue,newSensorValue); 
-      delay(1000);
-    } 
 
-    if (sensorValue >= threshold) 
-    {            
-      // LED blinks twice
-      digitalWrite(redLED, HIGH);
-      delay(150);
-      digitalWrite(redLED, LOW);
-      delay(150);
-      digitalWrite(redLED, HIGH);
-      delay(150);
-      digitalWrite(redLED, LOW);
-      // Play the alarm sound      
-      raiseAlarm();
-    }
-     else {
-    // Don't trigger the alarm
-    noTone(buzzerPin);
 
-    }  
-  }
+void readSensorValue()
+{
+
+        /* This function tend to solve the issue with the short blinking screen
+         *  When tft.fillScreen(),
+         */
+
+        //Read the current sensor value
+        sensorValue = analogRead(mq2sensorPin);
+        // Initialize the new and old sensor values
+
+
+        char newSensorValue[max_length] = {0};
+        delay(1000);
+        // Construct the new sensor value
+        sprintf(newSensorValue, "%lu", sensorValue);
+
+        // Compare the two strings to see if it has changed
+
+       // Display current gas concentation in air
+        tft.setFont(&OswaldRegular10pt7b);
+        tft.setCursor(15,40);
+        tft.println(" Atmospheric");
+        tft.println("     LPG Gas ");
+        tft.println("  Concentration");
+        tft.setCursor(40,150);
+        tft.println("ppm");
+
+
+        if (strcmp (oldSensorValue,newSensorValue) != 0)
+        {
+                tft.setFont(&OswaldRegular12pt7b);
+                // Change the text size
+                tft.setTextSize(1);
+                tft.setCursor(40,130);
+                // Change text color to current background color
+                tft.setTextColor(BLUE_COLOR);
+                // Print the old value that will overwrite our current text
+                tft.print(oldSensorValue);
+                //Bring the cursor back to the starting point
+                tft.setCursor(40,130);
+                //Change the text size
+                tft.setTextSize(1);
+                // Change the text color to the foreground color
+                tft.setTextColor(WHITE_COLOR);
+                //Print the new sensor value
+                tft.print(newSensorValue);
+                // Copy the new value to old value
+                strcpy(oldSensorValue,newSensorValue);
+                delay(1000);
+        }
+
+
+}
